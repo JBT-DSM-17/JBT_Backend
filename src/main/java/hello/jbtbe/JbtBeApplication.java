89209a5cc7765f915cbd6f53df3758a08896a377
bhyun <nbhyun0329@gmail.com>
2024-07-16 12:19:12 +0900

    INIT

---
+++ src/main/java/hello/jbtbe/JbtBeApplication.java
@@ -0,0 +1,13 @@
+package hello.jbtbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbtBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbtBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/jbtbe/JbtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.jbtbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbtBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
