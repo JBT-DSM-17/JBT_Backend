e6eaf8c3c0002a0dc9fe6735d34b84a33e1f1056
Daybreak312 <ty82afg12@gmail.com>
2024-07-16 17:20:23 +0900

    FIX :: Imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,9 +35,9 @@ dependencies {
 
     // Security
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-api")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson")
 
     // Validation
     implementation("org.springframework.boot:spring-boot-starter-validation")
