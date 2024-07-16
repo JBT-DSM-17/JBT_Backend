cfe0dc68835d6b8655510281ec9f9a9452620eb0
Daybreak312 <ty82afg12@gmail.com>
2024-07-16 19:47:34 +0900

    FEAT :: AccessDeniedHandler

---
+++ src/main/java/hello/jbtbe/global/security/access/CustomAccessDeniedHandler.java
@@ -0,0 +1,42 @@
+package hello.jbtbe.global.security.access;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.jbtbe.global.exception.ExceptionResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException {
+        System.out.println(accessDeniedException.getMessage());
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(
+                objectMapper.writeValueAsString(
+                        new ExceptionResponse(
+                                response.getStatus(),
+                                accessDeniedException.getMessage()
+                        )
+                )
+        );
+    }
+}
