5cc8db8a66dd45c2d2258accac3bc6c9b9e67c46
Daybreak312 <ty82afg12@gmail.com>
2024-07-16 22:42:50 +0900

    FEAT :: GoodsJpaEntity

---
+++ src/main/java/hello/jbtbe/domain/goods/entity/GoodsJpaEntity.java
@@ -0,0 +1,42 @@
+package hello.jbtbe.domain.goods.entity;
+
+import hello.jbtbe.domain.user.entity.UserJpaEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class GoodsJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String pictureUrl;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String locationDetail;
+
+    @Column(nullable = false)
+    private String detailUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserJpaEntity user;
+}
